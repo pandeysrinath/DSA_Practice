0d674ccd7b8035a66a8d2bd1b0991bbd508616fe
Srinath Pandey <pandeysrinath4@gmail.com>
2022-07-28 21:17:14 +0530

    Formatted Code

--- BinaryTree.java
+++ BinaryTree.java
@@ -6,10 +6,11 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class BinaryTree {
-    static int  idx = -1;
-    public static Node buildBinaryTree(int[] nodes){
+    static int idx = -1;
+
+    public static Node buildBinaryTree(int[] nodes) {
         idx++;
-        if(nodes[idx] == -1) {
+        if (nodes[idx] == -1) {
             return null;
         }
 
@@ -19,8 +20,8 @@ public class BinaryTree {
         return newNode;
     }
 
-    public static void inorderTraversal(Node root){
-        if(root == null){
+    public static void inorderTraversal(Node root) {
+        if (root == null) {
             return;
         }
 
@@ -29,8 +30,8 @@ public class BinaryTree {
         inorderTraversal(root.right);
     }
 
-    public static void preOrderTraversal(Node root){
-        if(root == null){
+    public static void preOrderTraversal(Node root) {
+        if (root == null) {
             return;
         }
 
@@ -39,8 +40,8 @@ public class BinaryTree {
         preOrderTraversal(root.right);
     }
 
-    public static void postOrderTraversal(Node root){
-        if(root == null){
+    public static void postOrderTraversal(Node root) {
+        if (root == null) {
             return;
         }
 
@@ -49,20 +50,20 @@ public class BinaryTree {
         System.out.print(root.data + " ");
     }
 
-    public static void levelOrderTraversal(Node root){
+    public static void levelOrderTraversal(Node root) {
         Queue<Node> queue = new ArrayDeque<>();
         queue.offer(root);
 
-        while(queue.size() > 0){
+        while (queue.size() > 0) {
             int count = queue.size();
-            for (;count > 0; count--){
+            for (; count > 0; count--) {
                 Node node = queue.poll();
                 assert node != null;
                 System.out.print(node.data + " ");
-                if(node.left != null){
+                if (node.left != null) {
                     queue.offer(node.left);
                 }
-                if (node.right != null){
+                if (node.right != null) {
                     queue.offer(node.right);
                 }
             }
@@ -70,16 +71,16 @@ public class BinaryTree {
         }
     }
 
-    public static void diagonalTraversal(Node root){
+    public static void diagonalTraversal(Node root) {
         Queue<Node> q = new ArrayDeque<>();
         q.offer(root);
 
-        while(q.size() > 0){
+        while (q.size() > 0) {
             int size = q.size();
-            while(size-- > 0){
+            while (size-- > 0) {
                 Node node = q.poll();
-                while(node != null){
-                    if(node.left != null){
+                while (node != null) {
+                    if (node.left != null) {
                         q.offer(node.left);
                     }
                     System.out.print(node.data + " ");
@@ -89,15 +90,15 @@ public class BinaryTree {
         }
     }
 
-    public static void rightViewOfBinaryTree(Node root){
-        if(root == null){
+    public static void rightViewOfBinaryTree(Node root) {
+        if (root == null) {
             return;
         }
 
         Queue<Node> queue = new ArrayDeque<>();
         queue.offer(root);
 
-        while(queue.size() > 0) {
+        while (queue.size() > 0) {
             int count = queue.size();
             System.out.print(queue.peek().data + " ");
             for (; count > 0; count--) {
@@ -113,57 +114,58 @@ public class BinaryTree {
         }
     }
 
-    public static void leftViewOfBinaryTree(Node root){
-        if(root == null){
+    public static void leftViewOfBinaryTree(Node root) {
+        if (root == null) {
             return;
         }
 
         Queue<Node> q = new ArrayDeque<>();
         q.offer(root);
 
-        while(q.size() > 0){
+        while (q.size() > 0) {
             int count = q.size();
             System.out.print(q.peek().data + " ");
-            for(int i = 0; i < count; i++){
+            for (int i = 0; i < count; i++) {
                 Node tn = q.poll();
 
-                if(tn.left != null){
+                if (tn.left != null) {
                     q.offer(tn.left);
                 }
 
-                if(tn.right != null){
+                if (tn.right != null) {
                     q.offer(tn.right);
                 }
             }
         }
     }
 
-    public static int getSize(Node root){
+    public static int getSize(Node root) {
         // Size of BT: Total number of nodes in Binary Tree
-        if(root == null)
+        if (root == null)
             return 0;
 
         return 1 + getSize(root.left) + getSize(root.right);
     }
 
-    public static int getSum(Node root){
+    public static int getSum(Node root) {
         // getSum returns the addition of all nodes in Binary Tree.
-        if (root == null){
+        if (root == null) {
             return 0;
         }
 
         return root.data + getSum(root.left) + getSum(root.right);
     }
 
-    public static int getHeight(Node root){
+    public static int getHeight(Node root) {
         // getHeight returns the number of edges present between root node and deepest leaf node.
-        if (root == null){
+        if (root == null) {
             return -1;
         }
 
         return 1 + Math.max(getHeight(root.left), getHeight(root.right));
     }
-    private static class Pair{
+
+    private static class Pair {
         Node node;
         int state;
 
@@ -173,7 +175,7 @@ public class BinaryTree {
         }
     }
 
-    public static void preInPostOrderTraversalIterative(Node root, ArrayList<Integer> preLst, ArrayList<Integer> inLst, ArrayList<Integer> postLst){
+    public static void preInPostOrderTraversalIterative(Node root, ArrayList<Integer> preLst, ArrayList<Integer> inLst, ArrayList<Integer> postLst) {
         if (root == null)
             return;
 
@@ -181,22 +183,22 @@ public class BinaryTree {
         Pair rtp = new Pair(root, 1);
         st.push(rtp);
 
-        while (st.size() > 0){
+        while (st.size() > 0) {
             Pair top = st.peek();
-            if(top.state == 1){
+            if (top.state == 1) {
                 preLst.add(top.node.data);
                 top.state++;
 
-                if (top.node.left != null){
+                if (top.node.left != null) {
                     Pair lp = new Pair(top.node.left, 1);
                     st.push(lp);
                 }
 
-            } else if(top.state == 2){
+            } else if (top.state == 2) {
                 inLst.add(top.node.data);
                 top.state++;
 
-                if (top.node.right != null){
+                if (top.node.right != null) {
                     Pair rp = new Pair(top.node.right, 1);
                     st.push(rp);
                 }
@@ -209,10 +211,10 @@ public class BinaryTree {
     }
 
 
-    public static ArrayList<Integer> boundary(Node node){
+    public static ArrayList<Integer> boundary(Node node) {
         ArrayList<Integer> ans = new ArrayList<>();
 
-        if(isLeaf(node)){
+        if (isLeaf(node)) {
             ans.add(node.data);
             return ans;
         }
@@ -224,11 +226,11 @@ public class BinaryTree {
         return ans;
     }
 
-    private static void leftBoundary(Node node, ArrayList<Integer> ans){
-        while(node != null) {
-            if(!isLeaf(node))
+    private static void leftBoundary(Node node, ArrayList<Integer> ans) {
+        while (node != null) {
+            if (!isLeaf(node))
                 ans.add(node.data);
-            if(node.left == null)
+            if (node.left == null)
                 node = node.right;
             else
                 node = node.left;
@@ -236,52 +238,52 @@ public class BinaryTree {
 
     }
 
-    private static void leafBoundary(Node node, ArrayList<Integer> ans){
-        if(node == null)
+    private static void leafBoundary(Node node, ArrayList<Integer> ans) {
+        if (node == null)
             return;
-        if(isLeaf(node))
+        if (isLeaf(node))
             ans.add(node.data);
 
         leafBoundary(node.left, ans);
         leafBoundary(node.right, ans);
     }
 
-    private static void rightBoundary(Node node, ArrayList<Integer> ans){
+    private static void rightBoundary(Node node, ArrayList<Integer> ans) {
 
         Stack<Integer> st = new Stack<>();
 
-        while(node != null) {
-            if(!isLeaf(node))
+        while (node != null) {
+            if (!isLeaf(node))
                 st.push(node.data);
-            if(node.right == null)
+            if (node.right == null)
                 node = node.left;
             else
                 node = node.right;
         }
 
-        while(st.size() > 0){
+        while (st.size() > 0) {
             ans.add(st.pop());
         }
     }
 
-   private static boolean isLeaf(Node node){
-        if(node != null && node.left == null && node.right == null)
+    private static boolean isLeaf(Node node) {
+        if (node != null && node.left == null && node.right == null)
             return true;
         else
             return false;
     }
 
-    static class DiaPair{
+    static class DiaPair {
         int ht;
         int dia;
     }
 
-    public static int diameter(Node root){
+    public static int diameter(Node root) {
         return diameter1(root).dia;
     }
 
-    private static DiaPair diameter1(Node root){
-        if(root == null){
+    private static DiaPair diameter1(Node root) {
+        if (root == null) {
             DiaPair bp = new DiaPair();
             bp.ht = -1;
             bp.dia = 0;
@@ -302,23 +304,23 @@ public class BinaryTree {
 
     public static ArrayList<Node> path;
 
-    public static boolean find(Node node, int data){
+    public static boolean find(Node node, int data) {
         if (node == null)
             return false;
 
-        if (node.data == data){
+        if (node.data == data) {
             path.add(node);
             return true;
         }
 
         boolean filc = find(node.left, data);
-        if (filc){
+        if (filc) {
             path.add(node);
             return true;
         }
 
         boolean firc = find(node.right, data);
-        if (firc){
+        if (firc) {
             path.add(node);
             return true;
         }
@@ -326,11 +328,11 @@ public class BinaryTree {
         return false;
     }
 
-    public static void printKthLevel(Node root, int k){
+    public static void printKthLevel(Node root, int k) {
         if (root == null || k < 0)
             return;
 
-        if (k == 0){
+        if (k == 0) {
             System.out.println(root.data);
         }
 
@@ -339,7 +341,7 @@ public class BinaryTree {
     }
 }
 
-class Main{
+class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         Node root = BinaryTree.buildBinaryTree(arr);
@@ -369,7 +371,7 @@ class Main{
 
         BinaryTree.path = new ArrayList<>();
         BinaryTree.find(root, 6);
-        for (Node i : BinaryTree.path){
+        for (Node i : BinaryTree.path) {
             int val = i.data;
             System.out.print(val + " -> ");
         }
